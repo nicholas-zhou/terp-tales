03c2dba4f0302d16871ad1c6789d4b6570d6103e
Nick Zhou <zhou.nicholas@outlook.com>
2023-09-02 16:27:34 -0400

    fetch reviews from planetterp api

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 async function fetchData() {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '[INSERT API KEY]',
+            'X-RapidAPI-Host': 'concerts-artists-events-tracker.p.rapidapi.com'
+        }
+    };
     
+    const res = await fetch('https://planetterp.com/api/v1/professor?name=Nelson Padua-Perez&reviews=true', options)
+    const record = await res.json()
+
 }
 fetchData(); 
